2d12be506f86d50c905da769b3b8e9a42ee6a669
aniketbidgar3 <aniketbidgar3@gmail.com>
2025-07-24 00:40:19 +0530

    "Java Array Practice"

---
+++ Array.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Array {
+
+    static void array(Scanner in) {
+        // Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+        array(in);
+
+    }
+}
